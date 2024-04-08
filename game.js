028c02a7c4f6bb4706206b95c9b128f49ae4bb27
Loc203 <tranlocmom@gmail.com>
2024-04-08 16:35:01 +0700

    update section

--- game.js
+++ game.js
@@ -1,3 +1,70 @@
+function section(board, col, row, value, firstActive) {
+  let PieceWidth = 42;
+  let PieceHeight = 52;
+  let div = document.createElement("div");
+  div.classList.add("section");
+  div.image = document.createElement("img");
+  div.setAttribute("col", col);
+  div.setAttribute("row", row);
+  div.setImage = function (imgIndex) {
+    this.image.src = "images/section" + imgIndex + ".png";
+    this.valueInMatrix = imgIndex;
+  };
+  if (col === firstActive?.col && row === firstActive?.row) {
+    div.style.opacity = "50%";
+  }
+
+  if (value > 0) {
+    div.style.cursor = "pointer";
+
+    div.setImage(value);
+  }
+  div.appendChild(div.image);
+
+  div.board = board;
+  div.colIndex = col;
+  div.rowIndex = row;
+
+  div.style.position = "absolute";
+  div.style.left = col * PieceWidth + "px";
+  div.style.top = row * PieceHeight + "px";
+  div.style.width = PieceWidth + "px";
+  div.style.height = PieceHeight + "px";
+
+  div.isVisible = true;
+
+  div.setVisible = function (flag) {
+    this.isVisible = flag;
+    this.style.visibility = flag ? "visible" : "hidden";
+  };
+
+  div.setBorder = function (thick, color) {
+    this.image.border = thick;
+    this.image.style.borderColor = color;
+  };
+
+  div.setHightlight = function () {
+    this.setBorder(1, "red");
+  };
+
+  div.setNormal = function () {
+    this.setBorder(1, "#009933");
+  };
+
+  div.onmouseover = function () {
+    this.setHightlight();
+  };
+
+  div.onmouseout = function () {
+    this.setNormal();
+  };
+  div.onclick = () => {};
+
+  div.setNormal();
+
+  return div;
+}
+
 let firstActive = null;
 let secondActive = null;
 
@@ -81,21 +148,56 @@ class Game {
     menu__btns.forEach((btn) => {
       btn.onclick = () => {
         const main__board__box = document.querySelector(".main__board__box");
-        const main__board__layer = document.querySelector(
-          ".main__board__layer"
-        );
+        const main__board__layer = document.querySelector(".main__board__layer");
         main__board__layer.style.opacity = "50%";
         main__board__layer.style.zIndex = 0;
         main__board__box.innerHTML = "";
         main__board__box.style.display = "none";
         this.renderMenuBoard();
+        this.init();
       };
     });
   }
   getIndexLevel() {
     return this.level - 1;
   }
+  reloadGame() {
+    clearInterval(timeInterval);
+    firstActive = null;
+    this.arrImages = this.algorithms.getNewMatrix();
+    this.blood = levels[this.getIndexLevel()].blood;
+    this.duration = levels[this.getIndexLevel()].duration;
+    this.loadImagesIcon();
+    this.renderLevelOption();
+    this.renderTitle();
+    this.renderLevel();
+    this.renderScore();
+    this.renderBlood();
+    this.renderTimeBar();
+    this.renderRound();
+    this.playAgain();
+    this.chooseRepair();
+    this.chooseHelp();
+    this.hideHelp();
+    this.chooseRenderMenu();
+  }
+  renderTimeBar() {
+    const timeBar = document.querySelector(".time__bar");
+    const timeText = document.querySelector(".time__text");
+    timeBar.style.height = this.duration / 2 + "px";
+    timeText.innerHTML = this.duration;
+    timeInterval = setInterval(() => {
+      this.duration--;
+      if (this.duration === 0) {
+        this.renderNoTime();
+      }
+      const timeBar = document.querySelector(".time__bar");
+      const timeText = document.querySelector(".time__text");
+      timeBar.style.height = this.duration / 2 + "px";
+      timeText.innerHTML = this.duration;
+    }, 1000);
+  }
 }
 
-const gameClass = new Game();
+const gameClass = new Game(section);
 gameClass.init();
